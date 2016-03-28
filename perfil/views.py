9eacb6ad58fa5cc7f6fa68587abb2f62a75e0295
BlisS <contacto@fixter.org>
2016-03-28 13:54:50 -0600

    perfil, login y loginsocial agregado y funcional en Debug

--- perfil/views.py
+++ perfil/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+class DashBoard(View):
+	@method_decorator(login_required)
+	def get(self,request):
+		template="perfil/dashboard.html"
+		context={
+		
+		}
+		return render(request,template,context)
