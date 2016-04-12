5fe0ddba76d62250a5aa1375fc9df42d1dc6d185
BlisS <contacto@fixter.org>
2016-04-11 22:43:31 -0500

    redes funcionando con img

--- perfil/models.py
+++ perfil/models.py
@@ -4,10 +4,17 @@ from django.conf import settings
 
 class UserProfile(models.Model):
 	user=models.OneToOneField(settings.AUTH_USER_MODEL)
-	photo=models.URLField()
+	photo=models.URLField(blank=True,null=True)
+	ide=models.CharField(max_length=50,null=True,blank=True)
+
+	def __str__(self):
+		return "Perfil de {}".format(self.user)
 
 class Correo(models.Model):
 	user=models.ForeignKey(settings.AUTH_USER_MODEL,related_name='correos')
 	correo=models.EmailField()
 	nombre=models.CharField(max_length=50)
+
+	def __str__(self):
+		return "Correo de {}".format(self.user)
 
--- perfil/pipelines.py
+++ perfil/pipelines.py
@@ -1,32 +1,23 @@
 # from django.core.files.base import ContentFile
 from requests import request, ConnectionError
 from .models import UserProfile
-from django.contrib.auth.models import User
+
 
 
 def save_profile_picture(backend, user, response, is_new,  *args, **kwargs):
-    '''
-    Get the user avatar (and any other details you're interested in)
-    and save them to the userprofile
-    '''
-    if backend.name == 'facebook' and is_new:
-        user_model=user
-        user_profile=UserProfile()
-        user_profile.user=user_model
+	if backend.name == 'facebook' and is_new:
+		user_model=user
+		user_profile=UserProfile()
+		user_profile.user=user_model
+	if user_profile.photo:
+		pass
+	else:
+		# url = 'http://graph.facebook.com/{0}/picture'.format(response['id'])
+		ide=response['id']
+		print(ide)
+		try:
+			user_profile.ide=ide
+			user_profile.save()
 
-        if user_profile.photo:
-            pass
-        else:
-            url = 'http://graph.facebook.com/{0}/picture'.format(response['id'])
-            try:
-                response = request('GET', url, params={'type': 'large'})
-                response.raise_for_status()
-            except ConnectionError:
-                pass
-            else:
-            	# user_profile.photo.save(u'',
-             #                     ContentFile(response.content),
-             #                     save=False
-             #                     )
-				user_profile.photo=url
-				user_profile.save()
+		except ConnectionError:
+			pass
