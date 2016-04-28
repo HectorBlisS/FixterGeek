692ea151f615720622c7b0433c0fb338f7e64e8c
BlisS <contacto@fixter.org>
2016-04-28 11:29:05 -0500

    probando tarea

---
+++ perfil/tasks.py
@@ -0,0 +1,10 @@
+from celery import task
+from django.core.mail import send_mail
+from django import settings
+
+@task
+def saludo(saludo):
+	subject='Probando Celery on aws'
+	message=saludo
+	mail_sent=send_mail(subject,message,'rotcehcm@hotmail.com',['contacto@fixter.org'])
+	return mail_sent
--- perfil/views.py
+++ perfil/views.py
@@ -4,6 +4,10 @@ from django.views.generic import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
+from .tasks import saludo
+
+
+
 class DashBoard(View):
 	@method_decorator(login_required)
 	def get(self,request):
@@ -11,4 +15,5 @@ class DashBoard(View):
 		context={
 		
 		}
+		saludo('Hola blissi')
 		return render(request,template,context)
