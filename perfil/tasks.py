dedd99bbb643168297b55f7bab91cafa357ebdd2
BlisS <contacto@fixter.org>
2016-04-28 14:12:02 -0500

    testing celery2 broker vars12

--- perfil/tasks.py
+++ perfil/tasks.py
@@ -2,6 +2,8 @@ from celery import task
 from django.core.mail import send_mail
 # from celery import shared_task
 
+
+
 @task
 def saludo(saludo):
 	print("morro")
@@ -10,3 +12,9 @@ def saludo(saludo):
 	mail_sent=send_mail(subject,message,'rotcehcm@hotmail.com',['contacto@fixter.org'])
 	
 	return mail_sent
+
+
+@task
+def sumale(num=2,num1=2):
+	res=num+num1
+	return res
