6725d8322f922cf12fd31f441542304d5066f56f
BlisS <contacto@fixter.org>
2016-05-12 14:41:37 -0500

    slug editado

--- eventos/views.py
+++ eventos/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
-from . import models
+from .models import Evento
 from django.shortcuts import get_object_or_404
 
 
@@ -8,15 +8,16 @@ from django.shortcuts import get_object_or_404
 class Todos(View):
 	def get(self,request):
 		template='eventos/todos.html'
-		eventos=models.Evento.objects.all()
+		eventos=Evento.objects.all()
 		context={
 			'events':eventos
 		}
 		return render(request,template,context)
+
 class DetalleEvento(View):
 	def get(self,request,slug):
 		template = "eventos/detalle.html"
-		evento = get_object_or_404(models.Evento,slug=slug)
+		evento = get_object_or_404(Evento,slug=slug)
 		context = {
 		'evento':evento,
 		}
