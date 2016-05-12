b6e7a64581cbb33d9bc401e5bcd66df993d76e0a
BlisS <contacto@fixter.org>
2016-05-12 15:44:12 -0500

    orden urls

--- eventos/urls.py
+++ eventos/urls.py
@@ -5,13 +5,15 @@ from . import views
 
 urlpatterns=[
 
+	url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$',
+		views.DetalleEvento.as_view(),
+		name="detalle"),
+
 	url(r'^todos/$',
 		views.Todos.as_view(),
 		name="todos"),
 
-	url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$',
-		views.DetalleEvento.as_view(),
-		name="detalle"),
+
 
 
 
