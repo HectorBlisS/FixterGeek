1b47f73c95accd0110f5e5100a538157712a2066
BlisS <contacto@fixter.org>
2016-05-13 00:43:30 -0500

    chet login3

--- geek/settings.py
+++ geek/settings.py
@@ -149,7 +149,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
 
-# LOGIN_REDIRECT_URL = reverse_lazy('dashboard')
+LOGIN_REDIRECT_URL = reverse_lazy('dashboard')
 LOGIN_URL = reverse_lazy('login')
 LOGOUT_URL = reverse_lazy('logout')
 
