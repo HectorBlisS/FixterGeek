e776f451a85e05cb36c353df996709f22ba4882a
BlisS <contacto@fixter.org>
2016-05-26 20:17:27 -0500

    tags

--- posts/templatetags/posts_tags.py
+++ posts/templatetags/posts_tags.py
@@ -2,7 +2,7 @@ from django import template
 register = template.Library()
 from ..models import Post
 
-@register.simple_tag
+@register.simple_tag(name='otro_nombre')
 def total_posts():
 	return Post.objects.all().count()
 
@@ -10,3 +10,7 @@ def total_posts():
 def show_posts(count=5):
 	los_posts = Post.objects.all().order_by('-titulo')[:count]
 	return {'posts':los_posts}
+
+@register.assignment_tag
+def todos_posts(count=3):
+	return Post.objects.all()[:count]
