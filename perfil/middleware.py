680ce0973ac311f1bb97815425ff829175e3434b
BlisS <contacto@fixter.org>
2016-06-08 13:08:29 -0500

    redirecting

--- perfil/middleware.py
+++ perfil/middleware.py
@@ -1,10 +1,11 @@
 from social.apps.django_app.middleware import SocialAuthExceptionMiddleware
-from django.shortcuts import HttpResponse
+# from django.shortcuts import HttpResponse
+from django.shortcuts import redirect
 from social import exceptions as social_exceptions
 
 class SocialAuthExceptionMiddleware(SocialAuthExceptionMiddleware):
 	def process_exception(self, request, exception):
 		if hasattr(social_exceptions, 'AuthCanceled'):
-			return HttpResponse("I'm the Pony %s" % exception)
+			return redirect('main:home')
 		else:
 			raise exception
