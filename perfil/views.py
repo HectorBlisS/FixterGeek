74ab2a05ba0a1338cfb8cbb9445c2c7cf8f0fb52
BlisS <contacto@fixter.org>
2016-07-06 11:01:05 -0500

    modificar info

--- perfil/views.py
+++ perfil/views.py
@@ -1,19 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import View
+
+from django.contrib import messages
 
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
 from .tasks import saludo
 
+from .forms import ProfileForm
 
 
 class DashBoard(View):
 	@method_decorator(login_required)
 	def get(self,request):
 		template="perfil/dashboard.html"
+		form = ProfileForm(instance=request.user)
+
 		context={
-		
+		'form':form,
 		}
 		# saludo.delay('Hola blissi')
 		return render(request,template,context)
+
+	def post(self,request):
+		if request.POST.get('updateInfo'):
+			form = ProfileForm(data = request.POST, instance = request.user)
+			print(form)
+			if form.is_valid():
+				form.save()
+				messages.success(request,'Tus datos fuerón actualizados')
+		else:
+			print('no se actualiza')
+		return redirect('dashboard')
+
+
+
+
+
