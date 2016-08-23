7885a2228abbc4aa4083d1852c16c3c83101290a
HectorBlisS <contacto@fixter.org>
2016-08-23 18:42:29 -0500

    listo

--- applys/admin.py
+++ applys/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Apply
 
 class ApplyAdmin(admin.ModelAdmin):
-	list_display = ['tel','tel2','path','motivos','beca','tipo','porque']
+	list_display = ['user','tel','tel2','path','motivos','beca','tipo','porque']
 	list_filter = ['path']
 
 
