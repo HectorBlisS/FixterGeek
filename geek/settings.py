8c92142d8c4deecde3f4b401cd56ac54868a7b33
HectorBlisS <contacto@fixter.org>
2016-08-23 18:40:27 -0500

    static

--- geek/settings.py
+++ geek/settings.py
@@ -151,7 +151,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR,"static"),)
 # STATIC_ROOT = 'staticfiles'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
