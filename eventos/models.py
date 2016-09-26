1f229f9090b5074a6527dd7d1450147cac97d2e5
Héctor Feliciano Campos Muñoz <BlisS@MBP-de-Hector.lan>
2016-09-26 08:49:50 -0500

    try to genericfy

--- eventos/models.py
+++ eventos/models.py
@@ -8,12 +8,14 @@ class Evento(models.Model):
 	descripcion=models.TextField()
 	detalles=models.CharField(max_length=140)
 	# precio = models.DecimalField(max_digits=10,decimal_places=2,default=0)
-	precio = models.CharField(max_length=10)
+	precio = models.IntegerField()
+	precio_promo=models.IntegerField()
 	fecha=models.DateTimeField()
 	direccion=models.TextField()
 	logo=models.ImageField(upload_to='logos',null=True,blank=True)
 	portada=models.ImageField(upload_to='eventos',null=True,blank=True)
 	boton=models.CharField(max_length=50,null=True,blank=True)
+	video=models.CharField(max_length=500, null=True,blank=True)
 
 	def __str__(self):
 		return self.titulo
