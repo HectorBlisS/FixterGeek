910aa028caa33f7ff9406d53788e15f592c50f50
BlisS <contacto@fixter.org>
2016-12-11 17:26:05 -0600

    debug test

--- geek/settings.py
+++ geek/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'iv5%rh+iwj1)&hv015rp#*&q+llqg52zihv$#ino%7x@)u!$20'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -89,7 +89,8 @@ WSGI_APPLICATION = 'geek.wsgi.application'
 # import dj_database_url
 # db_from_env = dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(db_from_env)
-if not DEBUG:
+# if not DEBUG:
+if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
