cb8de3847a6e4292d959220baa8a4190fbd954e0
BlisS <contacto@fixter.org>
2017-03-18 14:56:07 -0600

    modelo encuesta

--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, View
-
-
+from .forms import EncuestaForm
+from django.contrib import messages
 
 
 class newHome(TemplateView):
@@ -13,5 +13,16 @@ class Next(TemplateView):
 class Tour(TemplateView):
 	template_name="main/tour.html"
 
-class Encuesta(TemplateView):
-	template_name="main/encuesta.html"
+class Encuesta(View):
+	def get(self, request):
+		template_name="main/encuesta.html"
+		return render(request, template_name)
+
+	def post(self, request):
+		form = EncuestaForm(request.POST)
+		if form.is_valid():
+			form.save()
+			messages.success(request, "Gracias por tus comentarios")
+		return redirect('main:home')
+
+		
